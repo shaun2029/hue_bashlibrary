8f7c4c869371c302463501df5df5657748f67705
Shaun2029 <shauns2029@gmail.com>
2015-01-20 19:24:18 +0000

    Added toggle state and change brightness scripts.

---
+++ hue_change_brightness.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Demo for Hue BashLibrary (hue_bashlibrary.sh)
+# Written 2013 by Markus Proske, released under GNU GENERAL PUBLIC LICENSE v2, see LICENSE 
+# Google+: https://plus.google.com/+MarkusProske
+# Github: https://github.com/markusproske/hue_bashlibrary
+# -----------------------------------------------------------------------------------------
+
+
+# Note: this library relies on curl to be installed on your system.
+# Type which curl or curl --help in your Terminal to see if it is installed
+# If not, install with sudo apt-get install curl 
+
+
+# import my hue bash library
+source hue_bashlibrary.sh
+
+
+# CONFIGURATION
+# -----------------------------------------------------------------------------------------
+
+# Mind the gap: do not change the names of these variables, the bash_library needs those...
+
+ip='Philips-hue'								# IP of hue bridge
+devicetype='raspberry'						# Link with bridge: type of device
+username='huelibrary'						# Link with bridge: username / app name
+loglevel=1									# 0 all logging off, # 1 gossip, # 2 verbose, # 3 errors
+
+
+# Variables of this scripts
+light='1'								# Define the lights you want to use, e.g. '3' or '3 4' or '3 4 7 9'
+
+
+# PROGRAM FUNCTIONS
+# -----------------------------------------------------------------------------------------
+
+function usage {
+	# cmdname is defined in the library
+	echo "Usage: $cmdname [link | unlink | discover | config]"
+	echo "Or:    $cmdname light delta"
+	echo "light -   Light to change"
+	echo "delta -   Amount to change light brightness by."
+}
+
+
+
+# MAIN
+# -----------------------------------------------------------------------------------------
+
+# store name of command for usage and log
+# cmdname is defined in the library
+cmdname=`basename "$0"`
+
+
+# very simple argument processing
+if [[ $# == 1 ]]
+	then 
+	# valid number of arguments
+	if [[ $1 == "link" ]]
+	then
+		bridge_link
+	elif [[ $1 == "unlink" ]]
+	then
+		bridge_unlink
+	elif [[ $1 == "config" ]]
+	then	
+		bridge_config
+	elif [[ $1 == "discover" ]]
+	then
+		bridge_discover
+	else
+		usage	
+	fi
+	
+	echo		# force new line
+	exit
+else 
+	if (( $# > 2 )) || (( $# < 1 ))
+	then
+		# more than one argument, show usage
+		usage
+		echo
+		exit
+	fi
+fi 
+
+
+function change_brightness() {
+echo "Get Brightness..."
+    hue_get_brightness $2
+echo "Brightness: $result_hue_get_brightness"
+
+    if [ "$result_hue_get_brightness" != '' ]; then
+        BRIGHTNESS=$[result_hue_get_brightness + $1]
+        
+        if [ "$BRIGHTNESS" -gt "255" ]; then
+            BRIGHTNESS=255
+        fi
+
+        if [ "$BRIGHTNESS" -lt "0" ]; then
+            BRIGHTNESS=0
+        fi
+
+        if [ "$BRIGHTNESS" != "0" ]; then
+            hue_onoff "on" $light
+        fi
+        
+echo "Set Brightness: $BRIGHTNESS"
+        hue_setstate_brightness $BRIGHTNESS $2
+
+        if [ "$BRIGHTNESS" == "0" ]; then
+            hue_onoff "off" $light
+        fi
+    fi
+}
+
+# no arguments
+
+light=$1
+delta=$2
+
+change_brightness $delta $light
+
