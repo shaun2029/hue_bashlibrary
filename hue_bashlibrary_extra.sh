2e287061c9e30983246252804b2e4b80a57a384f
Shaun2029 <shauns2029@gmail.com>
2015-02-03 21:35:00 +0000

    Removed redundencey code - too slow.

--- hue_bashlibrary_extra.sh
+++ hue_bashlibrary_extra.sh
@@ -13,8 +13,17 @@ function set_brightness() {
         LEVEL=0
     fi
 
-    for i in `seq 1 3`;
+    hue_is_on $2
+
+    # Turn on light if brightness > 0
+    if [ "$result_hue_is_on" == 0 ]; then
+        hue_onoff "on" $2
+    fi
+
+    # Ensure light level 'takes'
+    for j in `seq 1 3`;
     do
+        hue_setstate_brightness $LEVEL $2
         hue_get_brightness $2
 
         if [ "$result_hue_get_brightness" == "$LEVEL" ]; then
@@ -22,44 +31,13 @@ function set_brightness() {
         fi
     done
 
-    # Already set to correct level then return
-    if [ "$result_hue_get_brightness" == "$LEVEL" ]; then
-        return
-    fi
-
-    # Turn on light
-    for i in `seq 1 3`;
-    do
+    # Turn off light if brightness = 0
+    if [ "$LEVEL" == "0" ]; then
         hue_is_on $2
 
-        if [ "$result_hue_is_on" == 0 ]; then
-            hue_onoff "on" $2
+        if [ "$result_hue_is_on" == 1 ]; then
+            hue_onoff "off" $2
         fi
-    done
-
-    for i in `seq 1 3`;
-    do
-        for j in `seq 1 30`;
-        do
-            hue_setstate_brightness $LEVEL $2
-            hue_get_brightness $2
-
-            if [ "$result_hue_get_brightness" == "$LEVEL" ]; then
-                break;
-            fi
-        done
-    done
-
-    # Turn off light if needed
-    if [ "$LEVEL" == "0" ]; then
-        for i in `seq 1 3`;
-        do
-            hue_is_on $2
-
-            if [ "$result_hue_is_on" == 1 ]; then
-                hue_onoff "off" $2
-            fi
-        done
     fi
 }
 
@@ -67,45 +45,32 @@ function set_brightness() {
 function change_brightness() {
     LEVEL=-1
 
-    # Do everything upto 3 times - seems to be inconsistancies
-    # Get the current light level
-    for i in `seq 1 3`;
-    do
-        hue_get_brightness $2
+    hue_get_brightness $2
 
-        if [ "$result_hue_get_brightness" != '' ]; then
-            if [ "$LEVEL" != "$result_hue_get_brightness" ]; then
-                LEVEL=$[result_hue_get_brightness]
-            else
-                break
-            fi
-        fi
-    done        
+    hue_is_on $1
 
-    if [ "$LEVEL" == "-1" ]; then
-        exit 1
+    # if the light is off the brightness level = 0
+    if [ "$STATE" == 1 ]; then
+    	if [ "$result_hue_get_brightness" != '' ]; then
+            LEVEL=$[result_hue_get_brightness]
+    	else
+            exit 1
+    	fi
+    else
+        LEVEL=0
     fi
-                 
-    # Get the new brightness level        
+
+    # Calculate the new brightness level
     NEWLEVEL=$[LEVEL + $1]
-    
+
     set_brightness $NEWLEVEL $2
 }
 
 function toggle_state() {
     STATE=-1
 
-    for i in `seq 1 3`;
-    do
-        hue_is_on $1
-        
-        if [ "$result_hue_is_on" != "$STATE" ]; then
-            STATE="$result_hue_is_on"
-        else
-            break
-        fi
-    done
-    
+    hue_is_on $1
+
     # Toggle state
     if [ "$STATE" == 1 ]; then
         STATE=0
@@ -113,19 +78,11 @@ function toggle_state() {
         STATE=1
     fi
 
-    for i in `seq 1 3`;
-    do
-        if [ $STATE == 0 ]; then
-            hue_onoff "off" $1
-        else
-            hue_onoff "on" $1
-        fi
-    
-        hue_is_on $1
+    if [ $STATE == 0 ]; then
+        hue_onoff "off" $1
+    else
+        hue_onoff "on" $1
+    fi
 
-        if [ "$result_hue_is_on" == $STATE ]; then
-            break
-        fi
-    done
+    hue_is_on $1
 }
-
